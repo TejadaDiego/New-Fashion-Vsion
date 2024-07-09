3c46a95d1f624745ad120f6e5d8b894e2e3ecbc0
Diego Tejada Espinal <Diego.tejada2006@gmail.com>
2024-07-09 13:01:30 -0500

    carrito

--- niños/carrito.js
+++ niños/carrito.js
@@ -49,3 +49,44 @@ function addItem() {
         alert("Por favor, escribe un elemento antes de agregar.");
     }
 }
+
+let cart = [];
+let total = 0;
+
+function toggleCart() {
+    const cartElement = document.getElementById('cart');
+    cartElement.style.display = cartElement.style.display === 'none' ? 'block' : 'none';
+}
+
+function addToCart(item, price) {
+    cart.push({ item, price });
+    total += price;
+    updateCart();
+}
+
+function updateCart() {
+    const cartItems = document.getElementById('cartItems');
+    const totalPrice = document.getElementById('totalPrice');
+    cartItems.innerHTML = '';
+    cart.forEach((product, index) => {
+        const li = document.createElement('li');
+        li.textContent = `${product.item} - S/${product.price.toFixed(2)}`;
+        cartItems.appendChild(li);
+    });
+    totalPrice.textContent = `Total: S/${total.toFixed(2)}`;
+}
+
+function clearCart() {
+    cart = [];
+    total = 0;
+    updateCart();
+}
+
+function purchaseItems() {
+    if (cart.length > 0) {
+        alert('El producto ha sido comprado con éxito');
+        clearCart();
+    } else {
+        alert('El carrito está vacío');
+    }
+}
